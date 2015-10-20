587b9eb17b906e91c51172a620463a06781c38f3
David Laing <david@stayup.io>
2015-10-20 17:24:47 +0000

    Default to *.name == "UNKNOWN" for apps / spaces / orgs / users that have been deleted

--- app.rb
+++ app.rb
@@ -17,24 +17,24 @@ clear_cache_every = ENV['CLEAR_CACHE_EVERY'] || 300
 
 def metadata(e)
   org_id = e[:organization_guid]
-  org_name = if @cache[org_id] then @cache[org_id] else @cache[org_id] = (@client.organization org_id).name end
+  org_name = @cache[org_id] ||= (@client.organization org_id).name rescue "UNKNOWN"
   space_id = e[:space_guid]
-  space_name = if @cache[space_id] then @cache[space_id] else @cache[space_id] = (@client.space space_id).name end
+  space_name = @cache[space_id] ||= (@client.space space_id).name rescue "UNKNOWN"
 
   actor_name = ""
   actor_id = e[:actor]
   if e[:actor_type] == "app"
-    actor_name = if @cache[actor_id] then @cache[actor_id] else @cache[actor_id] = (@client.app actor_id).name end
+      actor_name = @cache[actor_id] ||= (@client.app actor_id).name rescue "UNKNOWN"
   elsif e[:actor_type] == "user"
-    actor_name = if @cache[actor_id] then @cache[actor_id] else @cache[actor_id] = (@client.user actor_id).email end
+      actor_name = @cache[actor_id] ||= (@client.user actor_id).email rescue "UNKNOWN"
   end
 
   actee_name = ""
   actee_id = e[:actee]
   if e[:actee_type] == "app"
-    actee_name = if @cache[actee_id] then @cache[actee_id] else @cache[actee_id] = (@client.app actee_id).name end
+      actee_name = @cache[actee_id] ||= (@client.app actee_id).name rescue "UNKNOWN"
   elsif e[:actee_type] == "space"
-    actee_name = if @cache[actee_id] then @cache[actee_id] else @cache[actee_id] = (@client.space actee_id).name end
+      actee_name = @cache[actee_id] ||= (@client.space actee_id).name rescue "UNKNOWN"
   end
 
   data = {
@@ -46,10 +46,10 @@ def metadata(e)
 
   if e[:actor_type] == "app"
     data[:app_id] = actor_id
-    data[:app_name] = if @cache[actor_id] then @cache[actor_id] else @cache[actor_id] = (@client.app actor_id).name end
+    data[:app_name] = @cache[actor_id] ||= (@client.app actor_id).name rescue "UNKNOWN"
   elsif e[:actee_type] == "app"
     data[:app_id] = actee_id
-    data[:app_name] = if @cache[actee_id] then @cache[actee_id] else @cache[actee_id] = (@client.app actee_id).name end
+    data[:app_name] = @cache[actee_id] ||= (@client.app actee_id).name rescue "UNKNOWN"
   end
   data
 end
